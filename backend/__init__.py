14930785259b32b38234e53fd40a9b33d3f89740
Anuj Patel <patel.anuj21@gmail.com>
2022-02-12 16:47:55 -0800

    Dockerized the app with Gunicorn WSGI for production readiness

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,10 @@
 import os
 from flask import Flask
+from dotenv import load_dotenv
+
+parent_path = os.path.abspath('.')
+path=f'{parent_path}/.env'
+load_dotenv(dotenv_path=path,verbose=True)
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py', silent=True)
