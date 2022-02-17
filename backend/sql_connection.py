ddeb80d532a0aa77090c7282ca7fe8f0338c2ef1
Anuj Patel <patel.anuj21@gmail.com>
2022-02-16 23:55:04 -0800

    Exception handling when failing connections to the DB

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,5 +1,6 @@
 from distutils.command.config import config
 import mysql.connector
+from mysql.connector import errorcode
 from backend import app
 __cnx = None
 
@@ -10,5 +11,16 @@ def __init__(self):
 def get_sql_connection():
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user=app.config['DB_USER'], password=app.config['DB_PASSWORD'], host=app.config['DB_HOST'], database=app.config['DB_NAME'])
+        try:
+            __cnx = mysql.connector.connect(user=app.config['DB_USER'], password=app.config['DB_PASSWORD'], host=app.config['DB_HOST'], database=app.config['DB_NAME'])
+
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+        else:
+            _cnx.close()
     return __cnx
